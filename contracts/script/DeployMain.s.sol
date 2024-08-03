69823526e59d79b7502a2365669c002ec46d9c8c
alexsrebernic <84547550+alexsrebernic@users.noreply.github.com>
2024-08-03 20:39:47 -0300

    added deploy files

--- contracts/script/DeployMain.s.sol
+++ contracts/script/DeployMain.s.sol
@@ -6,6 +6,7 @@ import "forge-std/console.sol";
 
 import "../src/Caviar.sol";
 import "../src/StolenNftFilterOracle.sol";
+import {Mintory as RouterMintory} from "../src/Mintory.sol";
 
 contract DeployScript is Script {
     using stdJson for string;
@@ -13,12 +14,18 @@ contract DeployScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
 
         StolenNftFilterOracle s = new StolenNftFilterOracle();
-        Caviar c = new Caviar(address(s));
+        Caviar c = new Caviar(
+            address(s)
+        );
+        RouterMintory b = new RouterMintory(address(c));
 
         console.log("caviar:", address(c));
         console.log("stolen nft filter oracle:", address(s));
+        console.log("mintory:", address(b));
     }
 }
