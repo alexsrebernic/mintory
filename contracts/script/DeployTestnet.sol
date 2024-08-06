3749dc765e0a3e2a9eecf1d68afe5b860f05a510
alexsrebernic <84547550+alexsrebernic@users.noreply.github.com>
2024-08-06 08:04:16 -0300

    Added in deploy files uniswap integration

--- contracts/script/DeployTestnet.sol
+++ contracts/script/DeployTestnet.sol
@@ -17,10 +17,20 @@ contract DeployScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
+        address UNISWAP_FACTORY = vm.envAddress("UNISWAP_FACTORY_TEST");
+        address UNISWAP_ROUTER = vm.envAddress("UNISWAP_ROUTER_TEST");
+        address UNISWAP_POSITION_MANAGER = vm.envAddress("UNISWAP_POSITION_MANAGER_TEST");
+        address WETH = vm.envAddress("WETH_TEST");
+        address PRICEFEED = vm.envAddress("PRICEFEED_ETH_USD_TEST");
 
         StolenNftFilterOracle s = new StolenNftFilterOracle();
         Caviar c = new Caviar(
-            address(s)
+            address(s),
+            IUniswapV3Factory(UNISWAP_FACTORY),
+            ISwapRouter(UNISWAP_ROUTER),
+            INonfungiblePositionManager(UNISWAP_POSITION_MANAGER),
+            WETH,
+            PRICEFEED
         );
         RouterMintory b = new RouterMintory(address(c));
 
