3a3864fc0e854f88b9fdeb8a2e5f01e0f954aa4b
alexsrebernic <84547550+alexsrebernic@users.noreply.github.com>
2024-08-10 09:32:20 -0300

    add collect page

---
+++ mintory.fun/src/app/create/page.tsx
@@ -0,0 +1,16 @@
+import { Collection } from "@/components/collection";
+import { DisplayCard } from "@/components/DisplayCard/DisplayCard";
+import FilterBar from "@/components/FilterBar/FilterBar";
+import Hero from "@/components/Hero/Hero";
+import ImageTextBanner from "@/components/ImageTextBanner/ImageTextBanner";
+import Latest from "@/components/Latest/Latest";
+import PageSection from "@/components/PageSection/PageSection";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <Collection/>
+    </>
+  );
+}
