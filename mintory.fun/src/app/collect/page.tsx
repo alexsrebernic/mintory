527705ecc055db7c7b9d9f23dc09b17fc862ebd9
alexsrebernic <84547550+alexsrebernic@users.noreply.github.com>
2024-08-10 09:39:04 -0300

    Create add interface

--- mintory.fun/src/app/collect/page.tsx
+++ mintory.fun/src/app/collect/page.tsx
@@ -1,11 +1,5 @@
-import { Collection } from "@/components/collection";
-import { DisplayCard } from "@/components/DisplayCard/DisplayCard";
-import FilterBar from "@/components/FilterBar/FilterBar";
-import Hero from "@/components/Hero/Hero";
-import ImageTextBanner from "@/components/ImageTextBanner/ImageTextBanner";
-import Latest from "@/components/Latest/Latest";
-import PageSection from "@/components/PageSection/PageSection";
-import Image from "next/image";
+import { Collection } from "@/components/Collection/collection";
+
 
 export default function Home() {
   return (
--- mintory.fun/src/app/create/page.tsx
+++ mintory.fun/src/app/create/page.tsx
@@ -1,16 +1,9 @@
-import { Collection } from "@/components/collection";
-import { DisplayCard } from "@/components/DisplayCard/DisplayCard";
-import FilterBar from "@/components/FilterBar/FilterBar";
-import Hero from "@/components/Hero/Hero";
-import ImageTextBanner from "@/components/ImageTextBanner/ImageTextBanner";
-import Latest from "@/components/Latest/Latest";
-import PageSection from "@/components/PageSection/PageSection";
-import Image from "next/image";
+import { Create } from "@/components/Create/create";
 
 export default function Home() {
   return (
     <>
-    <Collection/>
+    <Create/>
     </>
   );
 }
